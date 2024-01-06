8cc6b97bd91b43ed0d7fc8546709d670d2c8c512
Juan Felipe R <42847785+Juferoga@users.noreply.github.com>
2024-01-06 12:45:18 -0500

    fix: notification process with url

--- docs/send_document.py
+++ docs/send_document.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 def send_telegram_message(chat_id, text, token):
     """Envía un mensaje de texto a un chat de Telegram."""
@@ -10,30 +11,13 @@ def send_telegram_message(chat_id, text, token):
     response = requests.post(url, data=payload)
     return response.json()
 
-def send_telegram_document(chat_id, document_path, token):
-    """Envía un documento a un chat de Telegram."""
-    url = f"https://api.telegram.org/bot{token}/sendDocument"
-    payload = {
-        "chat_id": chat_id
-    }
-    files = {
-        "document": open(document_path, "rb")
-    }
-    response = requests.post(url, data=payload, files=files)
-    return response.json()
+# Token del bot y ID del grupo desde variables de entorno
+bot_token = os.getenv('TELEGRAM_API_TOKEN')
+group_id = os.getenv('TELEGRAM_GROUP_ID')
 
-# Token del bot y ID del grupo
-bot_token = "6764398994:AAFKMbF5Quvm-arhMPerSiuBMye-xK1vhG8"
-group_id = "647870240"  # Reemplaza con el ID real de tu grupo
-
-# Mensaje a enviar
-message = "¡Hola, Grupo! Aquí está el documento."
+# Mensaje a enviar con enlace para descargar el artefacto
+artifact_url = os.getenv('ARTIFACT_URL')  # Necesitas configurar esto en GitHub Actions
+message = f"¡Hola, Grupo! Aquí está el documento: {artifact_url}"
 
 # Enviar mensaje de texto
 send_telegram_message(group_id, message, bot_token)
-
-# Ruta al archivo PDF (ajusta esto según tu configuración)
-pdf_path = "docs/main.pdf"
-
-# Enviar documento PDF
-send_telegram_document(group_id, pdf_path, bot_token)
